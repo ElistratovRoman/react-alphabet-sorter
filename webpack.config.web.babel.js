32b934d9e4bbe12219bb751e56ddef0a85208ef6
Roman <Roman@MacBook-Pro-Roman-2.local>
2016-11-19 01:47:15 +0300

    fix examples

--- webpack.config.web.babel.js
+++ webpack.config.web.babel.js
@@ -2,14 +2,12 @@
 import path from 'path'
 import webpack from 'webpack'
 
-const rootDir = process.cwd()
-
 const uglifyJs = new webpack.optimize.UglifyJsPlugin({
   compress: { warnings: false }
 })
 
 module.exports = {
-  entry: [path.join(rootDir, '/dev/index.html'), path.join(rootDir, '/dev/app.jsx')],
+  entry: [path.join(__dirname, '/dev/index.html'), path.join(__dirname, '/dev/app.jsx')],
 
   output: {
     path: __dirname,
